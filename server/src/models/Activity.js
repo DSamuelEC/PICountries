77973a63e5a9f5ca9b3e56cccd6b6dd357bd3553
DSamuelEC <samueledcardenas@gmail.com>
2023-08-25 22:07:13 -0500

    database check

---
+++ server/src/models/Activity.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Activity', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+          max: 5,
+        }
+    },
+    season:{
+        type: DataTypes.ENUM('Summer', 'Autumn', 'Winter', 'Spring'),
+        allowNull: false
+    }
+  },
+  {
+    timestamps: false
+  }
+  );
+};
